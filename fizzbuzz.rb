89987d2405f2143c182b41ea3b0fb673ddc26dcd
Kristen Robertson <kristen77@gmail.com>
2018-08-13 03:27:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  
+  if (theNumber is divisable by 3) and (theNumber is divisable by 5)
+    puts 
 end
