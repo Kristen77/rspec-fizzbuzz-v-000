d635e52d340042c9941db3df05793a8017e705a7
Kristen Robertson <kristen77@gmail.com>
2018-08-13 03:27:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,6 @@
 # See README.md for instructions on how to do this
 def fizzbuzz
   if (theNumber is divisable by 3) and (theNumber is divisable by 5)
-    puts 
+    puts "Fizzbuzz"
+    elsif 
 end
