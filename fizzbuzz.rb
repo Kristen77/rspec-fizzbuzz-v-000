0971e4dfd2e4eea6f4dd56635536be76b6438932
Kristen Robertson <kristen77@gmail.com>
2018-08-13 03:35:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  if 
+  if  fizz_3 = fizzbuzz(3)
     puts "Fizz"
     elsif fizzbuzz(5)
     puts "Buzz"
