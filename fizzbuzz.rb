f1224d22ae7f3665704187edd81fb1afca0f280d
Kristen Robertson <kristen77@gmail.com>
2018-08-13 04:12:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-`require "spec_helper"`
-def fizzbuzz
+
+def fizzbuzz(int)
   
   if theNumber / 3
     puts "Fizz"
