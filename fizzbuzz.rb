2af3e6bb15581ba5825f85dba8b665b7970c628e
Kristen Robertson <kristen77@gmail.com>
2018-08-13 03:34:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  if (theNumber is divisable by 3) and (theNumber is divisable by 5)
-    puts "Fizzbuzz"
-    elsif (theNumber is divisable by 3)
+  if fizzbuzz(3)
     puts "Fizz"
-  else (theNumber is divisable by 5)
-  puts "Buzz"
+    elsif fizzbuzz(5)
+    puts "Buzz"
+  else fizzbuzz(15)
+  puts "FizzBuzz"
 end
